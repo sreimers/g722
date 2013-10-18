833d6964ebeca7ef1ff8c3526e1c93077fadfb2a
Bendney Yu <bendney@gmail.com>
2013-10-18 11:58:19 +0800

    update all format

--- g722_interface.c
+++ g722_interface.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2011 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2011 The project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -20,7 +20,7 @@
 /************************Encoder function list***************************/
 WebRtc_Word16 WebRtcG722_CreateEncoder(G722EncInst **G722enc_inst)
 {
-    *G722enc_inst=(G722EncInst*)malloc(sizeof(g722_encode_state_t));
+    *G722enc_inst = (G722EncInst*)malloc(sizeof(g722_encode_state_t));
     if (*G722enc_inst!=NULL) {
       return(0);
     } else {
@@ -32,8 +32,8 @@ WebRtc_Word16 WebRtcG722_EncoderInit(G722EncInst *G722enc_inst)
 {
     // Create and/or reset the G.722 encoder
     // Bitrate 64 kbps and wideband mode (2)
-    G722enc_inst = (G722EncInst *) WebRtc_g722_encode_init(
-        (g722_encode_state_t*) G722enc_inst, 64000, 2);
+    G722enc_inst = (G722EncInst *)WebRtc_g722_encode_init(
+        (g722_encode_state_t*)G722enc_inst, 64000, 2);
     if (G722enc_inst == NULL) {
         return -1;
     } else {
@@ -62,7 +62,7 @@ WebRtc_Word16 WebRtcG722_Encode(G722EncInst *G722enc_inst,
 
 WebRtc_Word16 WebRtcG722_CreateDecoder(G722DecInst **G722dec_inst)
 {
-    *G722dec_inst=(G722DecInst*)malloc(sizeof(g722_decode_state_t));
+    *G722dec_inst = (G722DecInst*)malloc(sizeof(g722_decode_state_t));
     if (*G722dec_inst != NULL) {
       return (0);
     } else {
@@ -74,8 +74,8 @@ WebRtc_Word16 WebRtcG722_DecoderInit(G722DecInst *G722dec_inst)
 {
     // Create and/or reset the G.722 decoder
     // Bitrate 64 kbps and wideband mode (2)
-    G722dec_inst = (G722DecInst *) WebRtc_g722_decode_init(
-        (g722_decode_state_t*) G722dec_inst, 64000, 2);
+    G722dec_inst = (G722DecInst *)WebRtc_g722_decode_init(
+        (g722_decode_state_t*)G722dec_inst, 64000, 2);
     if (G722dec_inst == NULL) {
         return -1;
     } else {
@@ -95,8 +95,8 @@ WebRtc_Word16 WebRtcG722_Decode(G722DecInst *G722dec_inst,
 {
     // Decode the G.722 encoder stream
     *speechType=G722_WEBRTC_SPEECH;
-    return WebRtc_g722_decode((g722_decode_state_t*) G722dec_inst,
-            decoded, (WebRtc_UWord8*) encoded, len);
+    return WebRtc_g722_decode((g722_decode_state_t*)G722dec_inst,
+            decoded, (WebRtc_UWord8*)encoded, len);
 }
 
 WebRtc_Word16 WebRtcG722_Version(char *versionStr, short len)
